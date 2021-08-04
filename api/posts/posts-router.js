4fe244c48af2a87fc4e0ed52a3815daa41a0457d
michaelquintdev <michaelquintdev@gmail.com>
2021-08-04 16:40:45 -0500

    first get request done, server is setup

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,16 @@
 // implement your posts router here
+const express = require('express')
+const Post = require('./posts-model')
+const router = express.Router()
+
+// Starting our routes
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find(req.query)
+        res.status(200).json(posts)
+    } catch(error) {
+        res.status(500).json({message: `Nope bro ${error}`})
+    }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and launch it here
+const server = require('./api/server.js')
+
+server.listen(4000, () => {
+    console.log('\n*** Server Listening on http://localhost:4000 ***\n')
+})
